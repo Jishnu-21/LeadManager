155a0c482b714b2dbe13dae15edf73aee5804da8
Jishnu <jpjishnu21@gmail.com>
2024-11-25 15:13:23 +0530

    made changes for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000
+  },
+  base: '/'
 })
